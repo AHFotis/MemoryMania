bfe1c89ee83f7bf014bd4567b6285ca762499147
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-06-11 13:59:26 -0500

    Bootstrap installed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="home">
+    <div className="container home">
      <header>Memory Mania</header>
+     <button>Enter</button>
     </div>
   );
 }
