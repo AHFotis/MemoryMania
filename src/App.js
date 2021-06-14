62cb866c8233e34ed21c5433cf09f2122543c062
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-06-14 15:01:49 -0500

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Home from "./pages/Home"
-import "./App.css"
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Home from "./pages/Home";
+import "./App.css";
+
 
 function App() {
   return (
-    <div className="app">
-    <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+         <Home />
+        </Route>
+        <Route exact path="/game">
+         <Home />
+        </Route>
+        <Route>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
