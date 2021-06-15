2411dfb4e18c7edae93ebfa623f2024aca73a625
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-06-15 15:20:29 -0500

    Card Hooks set up

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {useSpring, animated as a} from "react-spring";
+//if you have problems, try chaning setFlipped to set
+function Card ({
+    id,
+    color,
+    game,
+    flippedCount,
+    setFlippedCount,
+    flippedIndexes,
+    setFlippedIndexes,
+}) {
+    const [flipped, setFlipped] = useState(false)
+    const {transform, opacity} = useSpring({
+        opacity: flipped ? 1 : 0,
+        transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg`,
+        config: {mass: 5, tension: 500, friction: 80},
+    })
 
-function Card () {
-    return <div>I will be a beautiful card one day</div>
+    useEffect(() => {
+        console.log('Flipped Indexes Changed')
+    }, [flippedIndexes])
+
+    const onCardClick = () => {
+        console.log('Card Clicked')
+        setFlipped(state => state)
+    }
+    return (
+    <div onClick={onCardClick}>
+        <a.div className="c back" style={{opacity: opacity.interpolate(o => 1 -o), transform,}} />
+        <a.div className="c front" style={{opacity, transform: transform.interpolate(t => `${t}rotateX(180deg)`), background: color,}} />
+    </div>
+    )
 }
 
 export default Card
